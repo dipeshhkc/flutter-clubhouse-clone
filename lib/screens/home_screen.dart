0605da8d214f34b1411dae772022ce7654f3d09d
Dipesh K.C <kcdipesh429@gmail.com>
2021-09-06 09:37:49 +0545

    feat:add home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clubhouse_ui/data.dart';
-import 'package:flutter_clubhouse_ui/widgets/widgets.dart';
+import 'package:flutter_clubhouse_ui/widgets/room.dart';
+import 'package:flutter_clubhouse_ui/widgets/upcoming_room.dart';
+import 'package:flutter_clubhouse_ui/widgets/user_profile.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -10,134 +12,45 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         leading: IconButton(
-          icon: const Icon(
-            CupertinoIcons.search,
-            size: 28.0,
-          ),
+          icon: const Icon(CupertinoIcons.search),
           onPressed: () {},
         ),
         actions: [
           IconButton(
-            icon: const Icon(
-              CupertinoIcons.envelope_open,
-              size: 26.0,
-            ),
+            icon: const Icon(CupertinoIcons.envelope_open),
             onPressed: () {},
           ),
           IconButton(
-            icon: const Icon(
-              CupertinoIcons.calendar,
-              size: 28.0,
-            ),
+            icon: const Icon(CupertinoIcons.calendar),
             onPressed: () {},
           ),
           IconButton(
-            icon: const Icon(
-              CupertinoIcons.bell,
-              size: 28.0,
-            ),
+            icon: const Icon(CupertinoIcons.bell),
             onPressed: () {},
           ),
           GestureDetector(
             onTap: () {},
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 10.0, 20.0, 10.0),
-              child:
-                  UserProfileImage(imageUrl: currentUser.imageUrl, size: 36.0),
-            ),
-          ),
+                padding: EdgeInsets.fromLTRB(8, 10, 20, 10),
+                child: UserProfile(
+                  image: currentUser.imageUrl,
+                  size: 36,
+                )),
+          )
         ],
       ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          ListView(
-            padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 120.0),
-            children: [
-              UpcomingRooms(upcomingRooms: upcomingRoomsList),
-              const SizedBox(height: 12.0),
-              ...roomsList.map((e) => RoomCard(room: e)),
-            ],
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              height: 100.0,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Theme.of(context).scaffoldBackgroundColor.withOpacity(0.1),
-                    Theme.of(context).scaffoldBackgroundColor,
-                  ],
-                ),
-              ),
+      body: ListView(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 120),
+          children: [
+            UpcomingRoom(
+              rooms: upcomingRoomsList,
             ),
-          ),
-          Positioned(
-            bottom: 60.0,
-            child: Container(
-              padding: const EdgeInsets.all(12.0),
-              decoration: BoxDecoration(
-                color: Theme.of(context).accentColor,
-                borderRadius: BorderRadius.circular(24.0),
-              ),
-              child: const Text.rich(
-                TextSpan(
-                  children: [
-                    WidgetSpan(
-                      child: Icon(
-                        CupertinoIcons.add,
-                        size: 21.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    TextSpan(
-                      text: ' Start a room',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 60.0,
-            right: 40.0,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(
-                    CupertinoIcons.circle_grid_3x3_fill,
-                    size: 28.0,
-                  ),
-                  onPressed: () {},
-                ),
-                Positioned(
-                  right: 4.6,
-                  bottom: 11.8,
-                  child: Container(
-                    height: 16.0,
-                    width: 16.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Theme.of(context).accentColor,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+            ...roomsList
+                .map((room) => SingleRoom(
+                      room: room,
+                    ))
+                .toList()
+          ]),
     );
   }
 }
